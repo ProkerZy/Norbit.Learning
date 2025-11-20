b7915a22834b1369e1fc40610a541046690df6dd
ProkerZy <saveliy.pen@bk.ru>
2025-11-21 02:32:37 +0300

    добавил пустой квадрат и метод с приветствием

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Введите число: ");
+            //Console.WriteLine("Введите число: ");
 
-            int num = int.Parse(Console.ReadLine());
-            DrawSquare(num);
-            Console.WriteLine();
-            GetOddNumbers(num);
+            //int num = int.Parse(Console.ReadLine());
+            //DrawHallowSquare(num);
+            CheckGreeting();
+
             Console.ReadLine();
         }
         static int[] GetOddNumbers(int num)
@@ -27,20 +27,63 @@
                     Console.Write(i + " ");
                 }
             }
-            
+
             return array;
         }
-        static void DrawSquare(int length)
+        static void DrawHallowSquare(int length)
         {
 
             for (int i = 0; i < length; i++)
             {
-                for (int j = 0; j < length; j++)
+                if (i == 0 || i == length - 1)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        Console.Write("X ");
+                    }
+                }
+                else
                 {
                     Console.Write("X ");
+                    for (int j = 1; j <= length - 2; j++) // вычитаем по символу с краев
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.Write("X");
                 }
                 Console.WriteLine();
             }
+        }
+        static void CheckGreeting()
+        {
+            Console.WriteLine("Поздоровайтесь!");
+            string anyWord = Console.ReadLine();
+            string neededWord = "hello";
+            int index = 0;
+
+            for (int i = 0; i < anyWord.Length; i++)
+            {
+                char current = anyWord[i];
+
+                if (current == neededWord[index])
+                {
+                    index++;
+                    if (index == 5)
+                    {
+                        break;
+                    }
+                }
+
+            }
+            if (index == 5)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+                
         }
     }
 }
